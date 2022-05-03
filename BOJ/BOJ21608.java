91dc1d20f7168980fe98bcfebeadbf22ae5a48fa
kimensoo <ekclstkfka5@gmail.com>
2022-05-03 13:19:52 +0900

    백준 21608 상어초등학교 Main.java add

--- BOJ/BOJ21608.java
+++ BOJ/BOJ21608.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class BOJ21608 {
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     static BufferedReader br;
